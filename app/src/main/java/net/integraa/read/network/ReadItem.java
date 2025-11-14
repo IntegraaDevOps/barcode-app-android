c72d4b5ed97d4acf2c9c6e920e388e319404ee6f
IntegraaDevOps <it@integraa.it>
2025-11-14 08:34:43 +0100

    Add reads

---
+++ app/src/main/java/net/integraa/read/network/ReadItem.java
@@ -0,0 +1,19 @@
+package net.integraa.read.network;
+
+public class ReadItem {
+    private String read;
+    private String date;
+    private String serial;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getRead() {
+        return read;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+}
